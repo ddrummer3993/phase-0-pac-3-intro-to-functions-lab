cda1b9b757af8135fd6724f782587459c3aada21
David Drummer <david.b.drummer@gmail.com>
2022-01-23 18:39:17 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+//Shout function
+function shout(string) {
+    return string.toUpperCase();
+}
+//whisper function
+function whisper(string) {
+    return string.toLowerCase();
+}
+//log shout
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+// log whisper
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+//say hi to headphoned roommate
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return 'I can\'t hear you!';
+    } else if (string === string.toUpperCase()) {
+        return 'YES INDEED!';
+    } else if (string === 'Let\'s have dinner together!'){
+        return 'I would love to!';
+    }
+}
+
+console.log(sayHiToHeadphonedRoommate('Let\'s have dinner together!'));
